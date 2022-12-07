947fcf80218d87263e58ee2e4cc77a866cb1269a
zakaria <zakariasoukarieh@gmail.com>
2022-12-07 18:46:34 +0200

    step4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step4/step4.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var name = document.getElementById("name").value;
+    var surname= document.getElementById("surname").value;
+    var city = document.getElementById("city").value;
+ 
+    alert('Hi ' + name + " " + surname + " " + city );
+  }
