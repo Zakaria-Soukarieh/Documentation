09861f06b0a2dda37f8013fec7471785ec62ce43
zakaria <zakariasoukarieh@gmail.com>
2022-12-07 19:08:36 +0200

    step6

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/step6/step6.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var first = document.getElementById("first_number").value;
+    var second = document.getElementById("second_number").value;
+    var answer = first / second
+    alert( answer );
+  }
