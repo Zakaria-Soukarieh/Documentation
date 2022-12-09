d6c048c34f811fdbef7cad90df7bfc9f618be0ed
zakaria <zakariasoukarieh@gmail.com>
2022-12-09 13:21:21 +0200

    step2

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 2/step2.js
@@ -0,0 +1,8 @@
+function toggle(x){
+  if (x==1) {
+    document.getElementById("texte").style.display = "block"
+  };
+  if (x==2) {
+    document.getElementById("texte").style.display = "none"
+  };
+};
