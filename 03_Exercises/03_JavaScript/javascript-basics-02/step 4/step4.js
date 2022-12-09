cfb821ded5ab3057488568f97223f3e3a5374eea
zakaria <zakariasoukarieh@gmail.com>
2022-12-09 09:58:05 +0200

    step4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 4/step4.js
@@ -0,0 +1,15 @@
+
+function confirm_reset() {
+    return confirm("Are you sure you want to reset all text?");
+
+    var getValue= document.getElementById("name");
+          getValue.value = "";
+      
+      var getValue= document.getElementById("city");
+          getValue.value = "";
+      
+      var getValue= document.getElementById("surname");
+
+          getValue.value = "";
+      }
+
