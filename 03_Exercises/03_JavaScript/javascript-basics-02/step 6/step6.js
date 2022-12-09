9aac605712c96d06dae090c009df528b2538f6c5
zakaria <zakariasoukarieh@gmail.com>
2022-12-09 10:56:02 +0200

    step6

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 6/step6.js
@@ -0,0 +1,32 @@
+function changeimg1(x) {
+    if(x==1){
+    document.getElementById("image1").src="/images/image1.jpg";}
+    if(x==2){
+
+    document.getElementById("image2").src="/images/image2.jpg";}
+    if(x==3){
+
+    document.getElementById("image3").src="/images/image3.jpg";}
+    if(x==4){
+
+    document.getElementById("image4").src="/images/image4.jpg";}
+    if(x==5){
+
+    document.getElementById("image5").src="/images/image5.jpg";}
+};
+function backimg(x) {
+    if(x==1){
+    document.getElementById("image1").src="/images/image1_2.jpg";}
+    if(x==2){
+
+    document.getElementById("image2").src="/images/image2_2.jpg";}
+    if(x==3){
+
+    document.getElementById("image3").src="/images/image3_2.jpg";}
+    if(x==4){
+
+    document.getElementById("image4").src="/images/image4_2.jpg";}
+    if(x==5){
+
+    document.getElementById("image5").src="/images/image5_2.jpg";}
+};
