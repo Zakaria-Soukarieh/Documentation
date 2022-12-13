cdebf3eaa6a5077a47a9bb645b8569af517921f7
zakaria <zakariasoukarieh@gmail.com>
2022-12-13 12:11:14 +0200

    Basic Queries

---
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -0,0 +1,9 @@
+1. select name from students
+2.SELECT * from students WHERE Age > 30
+3.SELECT * from students WHERE Age > 30 AND Gender = 'F'
+4.SELECT Points FROM students WHERE name = 'Alex'
+5.INSERT INTO students (ID, name, Age, Gender, Points) VALUES (7, 'zak', 19, 'M', 200)
+6.UPDATE students SET Points = 750 WHERE name = 'Basma'
+7.UPDATE students SET Points = 100 WHERE name = 'Alex'
+
+
